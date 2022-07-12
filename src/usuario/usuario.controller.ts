fbd7f6604eb074dc9330a6c3558aba700c8554e5
Vinicius Rosa <viniciusrosa3525@gmail.com>
2022-07-12 14:54:46 -0300

    Initial commit

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+
+@Controller('users')
+export class UsuarioController {
+  private usuarioService = new UsuarioService();
+
+  @Post()
+  public cria(@Body() usuario) {
+    const usuarioCriado = this.usuarioService.cria(usuario);
+
+    return usuarioCriado;
+  }
+}
