ab9dc7a1fcecd2b56b29d18399921476df079313
Vinicius Rosa <viniciusrosa3525@gmail.com>
2022-07-13 21:27:03 -0300

    New domain

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Controller('users')
 export class UsuarioController {
-  private usuarioService = new UsuarioService();
+  constructor(private usuarioService: UsuarioService) {}
 
   @Post()
-  public cria(@Body() usuario) {
+  public cria(@Body() usuario: Usuario): Usuario {
     const usuarioCriado = this.usuarioService.cria(usuario);
 
     return usuarioCriado;
+  }
+
+  @Get(':nomeDeUsuario')
+  public buscaPorNomeDeUsuario(
+    @Param('nomeDeUsuario') nomeDeUsuario: string,
+  ): Usuario {
+    const usuarioEncontrado =
+      this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario);
+    return usuarioEncontrado;
   }
 }
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  private usuarios = [];
+  private usuarios: Array<Usuario> = [
+    {
+      id: 1,
+      nomeDeUsuario: 'usuario1',
+      email: 'usuario@exemplo.com',
+      senha: 'usuario-exemplo',
+      nomeCompleto: 'Novo usuario exemplo',
+      dataDeEntrada: new Date(),
+    },
+  ];
 
-  public cria(usuario) {
+  public cria(usuario: Usuario): Usuario {
     this.usuarios.push(usuario);
 
     return usuario;
+  }
+
+  public buscaPorNomeDeUsuario(nomeDeUsuario: string): Usuario {
+    return this.usuarios.find(
+      (usuario) => usuario.nomeDeUsuario === nomeDeUsuario,
+    );
   }
 }
