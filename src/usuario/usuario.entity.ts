1ab2327f9cf9462391ee6f22611842c7c284e001
Vinicius Rosa <viniciusrosa3525@gmail.com>
2022-07-17 15:17:41 -0300

    Validation of objects

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,33 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class Usuario {
   id: number;
+
+  @IsNotEmpty({
+    message: 'nomeDeUsuario é obrigatório.',
+  })
+  @IsString({
+    message: 'nomeDeUsuario precisa ser uma string.',
+  })
   nomeDeUsuario: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'email precisa ser um endereço de email válido.',
+    },
+  )
   email: string;
+
+  @IsNotEmpty({
+    message: 'senha é obrigatório.',
+  })
   senha: string;
+
+  @IsNotEmpty({
+    message: 'nomeCompleto é obrigatório.',
+  })
   nomeCompleto: string;
+
   dataDeEntrada: Date;
 }
